3de5a81027eff08f4cbcd1f791945daefe177a3e
Marko Ratkaj <marko.ratkaj@sartura.hr>
2018-09-18 16:34:51 +0200

    kernel-version.mk: add kernel 4.19 support
    Signed-off-by: Marko Ratkaj <marko.ratkaj@sartura.hr>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,10 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .123
 LINUX_VERSION-4.9 = .131
 LINUX_VERSION-4.14 = .74
+LINUX_VERSION-4.19 = .0
 
 LINUX_KERNEL_HASH-3.18.123 = c10de32c9b31fb619b016a00d77afc394db5a4542e258e927f06a5ead86f8c64
 LINUX_KERNEL_HASH-4.9.131 = 57db3295469d3990af93703a97b07f1e8a14920bf3f5b68ce0330a02e7aa5d60
 LINUX_KERNEL_HASH-4.14.74 = 70e69189fd7b2f96fa33833735c7bcf3281dc0172c3734e2c5819e9d57015c72
+LINUX_KERNEL_HASH-4.19.0 = aaae6a07e00f14b919c351e836029eb4f0df75d2aafa471b469f58e49b705122
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
